8597e23d57df14284d2b9f6d210ca78c04365c54
Murillo Mtz <murillo.santos@ucsal.edu.br>
2020-07-03 09:40:27 -0300

    Testando a pirmeira class de dominium

--- src/main/java/com/mtz/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/mtz/cursomc/resources/CategoriaResource.java
@@ -1,17 +1,30 @@
 package com.mtz.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.mtz.cursomc.domain.Categoria;
+
 @RestController
-@RequestMapping(value="/categorias")
-public class CategoriaResource  {
-	
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "rest esta funcionando!";
+	public List<Categoria> listar() {
+
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+
+		List<Categoria> lista = new ArrayList<>();
+
+		lista.add(cat1);
+		lista.add(cat2);
+
+		return lista;
 	}
-	
-	
+
 }
