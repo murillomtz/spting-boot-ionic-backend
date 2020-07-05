c9574b8a2af73369018f94e513a3c966269743f4
Murillo Mtz <murillo.santos@ucsal.edu.br>
2020-07-04 22:32:23 -0300

    Criando operações de instaciação

--- src/main/java/com/mtz/cursomc/CursomcApplication.java
+++ src/main/java/com/mtz/cursomc/CursomcApplication.java
@@ -1,13 +1,34 @@
 package com.mtz.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.mtz.cursomc.DAO.CategoriaRepository;
+import com.mtz.cursomc.domain.Categoria;
+
 @SpringBootApplication
-public class CursomcApplication {
+//implements faz executar a instaciação no momento em que compilar
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria cat1 = new Categoria(null, "Informatica");
+		Categoria cat2 = new Categoria(null, "Escritorio");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+
 	}
 
 }
