fa3162545560720b5df3d70918ac61e1b2299e2f
Murillo Mtz <murillo.santos@ucsal.edu.br>
2020-07-08 22:08:27 -0300

    implementando metodo post

--- src/main/java/com/mtz/cursomc/resources/CategoriaResource.java
+++ src/main/java/com/mtz/cursomc/resources/CategoriaResource.java
@@ -1,15 +1,16 @@
 package com.mtz.cursomc.resources;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.net.URI;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.mtz.cursomc.domain.Categoria;
 import com.mtz.cursomc.services.CategoriaService;
@@ -21,6 +22,7 @@ public class CategoriaResource {
 	@Autowired // Para instaciar altomaticamente
 	private CategoriaService service;
 
+	/* METODO PARA EFETUAR A OPERALÇAO DE GET, PASSANDO ID E RECEBER CATEGARIA */
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	// @PathVariable significa q o id passado pelo requeste mappin vai ser u sado no
 	// metodo, como se fosse um "this."
@@ -28,6 +30,15 @@ public class CategoriaResource {
 
 		Optional<Categoria> obj = service.buscar(id);
 		return ResponseEntity.ok().body(obj);
+	}
+
+	/* METODO PARA RECEBER UMA CATEGORIA E INSETIR NO DB */
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@RequestBody Categoria obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+
 	}
 
 }
