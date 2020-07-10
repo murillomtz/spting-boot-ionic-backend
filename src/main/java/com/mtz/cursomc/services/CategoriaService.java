147e227b8eaef01a801e11dcf28259696209a235
Murillo Mtz <murillo.santos@ucsal.edu.br>
2020-07-10 09:55:43 -0300

    Deletando uma categoria e tratando exce

--- src/main/java/com/mtz/cursomc/services/CategoriaService.java
+++ src/main/java/com/mtz/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.mtz.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.mtz.cursomc.DAO.CategoriaRepository;
 import com.mtz.cursomc.domain.Categoria;
+import com.mtz.cursomc.services.exceptions.DataIntegrityException;
 import com.mtz.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,16 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+
+		try {
+			repo.deleteById(id);
+
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possivel excluir uma categoria que possui produtos");
+		}
 	}
 }
