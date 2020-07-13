b5949d664b89399dbd4c6223b382a363f275fc05
Murillo Mtz <murillo.santos@ucsal.edu.br>
2020-07-13 15:52:43 -0300

    Email não repetido na sessão de cliente

--- src/main/java/com/mtz/ped/DAO/ClienteRepository.java
+++ src/main/java/com/mtz/ped/DAO/ClienteRepository.java
@@ -1,5 +1,7 @@
 package com.mtz.ped.DAO;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,8 @@ import com.mtz.ped.domain.Cliente;
 @Repository // Extenção inplementa os comando como
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	// Fazer uma busca por e-mail
+
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);// O spring entende e completa o metodo que retorno um cliente e recebe um email
 }
--- src/main/java/com/mtz/ped/services/validation/ClienteInsertValidator.java
+++ src/main/java/com/mtz/ped/services/validation/ClienteInsertValidator.java
@@ -6,12 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.mtz.ped.DAO.ClienteRepository;
+import com.mtz.ped.domain.Cliente;
 import com.mtz.ped.domain.enums.TipoCliente;
 import com.mtz.ped.dto.ClienteNewDTO;
 import com.mtz.ped.resources.exception.FieldMessage;
 import com.mtz.ped.services.validation.utils.BR;
 
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository repo;
+
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -25,6 +33,11 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 		}
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidTfn(objDto.getCpfOuCnpj())) {
 			list.add(new FieldMessage("cpfOuCnpj", "CNPJ invalido"));
+		}
+
+		Cliente aux = repo.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
 		}
 		for (FieldMessage e : list) {
 			context.disableDefaultConstraintViolation();
