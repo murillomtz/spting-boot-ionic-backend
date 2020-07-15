a0314288d71f9ccf346b4e305b7040ae6b975b0e
Murillo Mtz <murillo.santos@ucsal.edu.br>
2020-07-14 21:47:39 -0300

    Subtotal e total de Pedido

--- src/main/java/com/mtz/ped/domain/Pedido.java
+++ src/main/java/com/mtz/ped/domain/Pedido.java
@@ -60,6 +60,14 @@ public class Pedido implements Serializable {
 		this.enderecoDeEntrega = enderecoDeEntrega;
 	}
 
+	public double getValorTotal() {
+		double soma = 0;
+		for (ItemPedido ip : itens) {
+			soma = soma + ip.getSubTotal();
+		}
+		return soma;
+	}
+
 	public Integer getId() {
 		return id;
 	}
