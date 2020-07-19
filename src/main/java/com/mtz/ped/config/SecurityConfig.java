5c45ef43cca4eb3e701efc3d0a8698dce4856b50
Murillo Mtz <murillo.santos@ucsal.edu.br>
2020-07-19 17:30:16 -0300

    Implementando autenticacao e geracao do token JWT

--- src/main/java/com/mtz/ped/config/SecurityConfig.java
+++ src/main/java/com/mtz/ped/config/SecurityConfig.java
@@ -7,14 +7,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.mtz.ped.security.JWTAuthenticationFilter;
+import com.mtz.ped.security.JWTUtil;
 
 @Configuration
 @EnableWebSecurity
@@ -22,6 +27,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private Environment env;
+
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+	private JWTUtil jwtUtil;
 
 	// Caminhos liberados
 	private static final String[] PUBLIC_MATCHERS = {
@@ -32,13 +43,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	// Apenas para recuperar
 	private static final String[] PUBLIC_MATCHERS_GET = {
 
-			"/produtos/**", "/categorias/**", "/estados/**","/clientes/**"
+			"/produtos/**", "/categorias/**", "/estados/**", "/clientes/**"
 
 	};
 
-	private static final String[] PUBLIC_MATCHERS_POST = { 
-			
-			
+	private static final String[] PUBLIC_MATCHERS_POST = {
+
 			"/clientes/**", "/auth/forgot/**"
 
 	};
@@ -46,13 +56,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 
-		// Liberar o acesso ao banco H2
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
+
 		http.cors().and().csrf().disable();
-		http.authorizeRequests().antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
-				.antMatchers(PUBLIC_MATCHERS).permitAll().anyRequest().authenticated();
+		http.authorizeRequests().antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+				.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll().antMatchers(PUBLIC_MATCHERS).permitAll()
+				.anyRequest().authenticated();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		//http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 
@@ -63,6 +76,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", configuration);
 		return source;
+	}
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder());
+
 	}
 
 	@Bean
---
+++ src/main/java/com/mtz/ped/services/userDetailsServiceImpl.java
@@ -0,0 +1,29 @@
+package com.mtz.ped.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.mtz.ped.DAO.ClienteRepository;
+import com.mtz.ped.domain.Cliente;
+import com.mtz.ped.security.UserSS;
+
+@Service
+public class userDetailsServiceImpl implements UserDetailsService {
+
+	@Autowired
+	private ClienteRepository repo;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Cliente cli = repo.findByEmail(email);
+
+		if (cli == null) {
+			throw new UsernameNotFoundException(email);
+		}
+		return new UserSS(cli.getId(), cli.getEmail(), cli.getSenha(), cli.getPerfis());
+	}
+
+}
