0e4703b152a02288c042dc138dbd81772cd296e2
Murillo Mtz <murillo.santos@ucsal.edu.br>
2020-07-20 18:59:03 -0300

    Refresh token

---
+++ src/main/java/com/mtz/ped/resources/AuthResource.java
@@ -0,0 +1,39 @@
+package com.mtz.ped.resources;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mtz.ped.security.JWTUtil;
+import com.mtz.ped.security.UserSS;
+import com.mtz.ped.services.UserService;
+
+@RestController
+@RequestMapping(value = "/auth")
+public class AuthResource {
+
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	@Autowired
+	// private AuthService service;
+
+	@RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+	public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+		UserSS user = UserService.authenticated();
+		String token = jwtUtil.generateToken(user.getUsername());
+		response.addHeader("Authorization", "Bearer " + token);
+		response.addHeader("access-control-expose-headers", "Authorization");
+		return ResponseEntity.noContent().build();
+	}
+//
+//	@RequestMapping(value = "/forgot", method = RequestMethod.POST)
+//	public ResponseEntity<Void> forgot(@Valid @RequestBody EmailDTO objDto) {
+//		service.sendNewPassword(objDto.getEmail());
+//		return ResponseEntity.noContent().build();
+//	}
+}
